b3bd3ad11b0d8db8ef8624c1c49dd5b57b4cbe35
VihanR <71479823+VihanR@users.noreply.github.com>
2021-09-07 20:32:15 +1000

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.473ea9737532c59108bfc0da03cca169.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93243e7bc2a045af67a1849f389c537",
+    "url": "/VihanR.github.io/index.html"
+  },
+  {
+    "revision": "65e67c3592458d0e10c3",
+    "url": "/VihanR.github.io/static/css/2.2273db08.chunk.css"
+  },
+  {
+    "revision": "8f313223366f89b070ae",
+    "url": "/VihanR.github.io/static/css/main.91061dab.chunk.css"
+  },
+  {
+    "revision": "65e67c3592458d0e10c3",
+    "url": "/VihanR.github.io/static/js/2.3be8f285.chunk.js"
+  },
+  {
+    "revision": "4effa9de81938b2545f40d4f7c3a8025",
+    "url": "/VihanR.github.io/static/js/2.3be8f285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f313223366f89b070ae",
+    "url": "/VihanR.github.io/static/js/main.073cea12.chunk.js"
+  },
+  {
+    "revision": "eddd1b04894b570be2df",
+    "url": "/VihanR.github.io/static/js/runtime~main.f56c6cf8.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/VihanR.github.io/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/VihanR.github.io/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/VihanR.github.io/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/VihanR.github.io/static/media/slick.f97e3bbf.svg"
+  }
+]);
