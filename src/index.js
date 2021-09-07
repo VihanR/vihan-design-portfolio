87debf55dd9789959311d561e06f80b6ce6b1b39
VihanR <71479823+VihanR@users.noreply.github.com>
2021-09-07 20:52:28 +1000

    Create a React app and publish it to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -65,11 +65,6 @@ import Button from "./blocks/Button";
 import { BrowserRouter, Switch, Route  } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
 
 class Root extends Component{
     render(){
