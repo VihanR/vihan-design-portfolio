871680cfe8c3f442b0d75fe62f468c4dee53f304
VihanR <71479823+VihanR@users.noreply.github.com>
2021-09-08 20:54:44 +1000

    first commit

---
+++ build/precache-manifest.e30f76239581ca228d25e0ff42e4156d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49838955f3ffdf0f10eb799d6581b9b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c487c2fd7bedcb78d42",
+    "url": "/static/css/2.f0ad1888.chunk.css"
+  },
+  {
+    "revision": "37c9eb402005a1c6cb3c",
+    "url": "/static/css/main.91061dab.chunk.css"
+  },
+  {
+    "revision": "7c487c2fd7bedcb78d42",
+    "url": "/static/js/2.3be8f285.chunk.js"
+  },
+  {
+    "revision": "4effa9de81938b2545f40d4f7c3a8025",
+    "url": "/static/js/2.3be8f285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c9eb402005a1c6cb3c",
+    "url": "/static/js/main.f248f3ef.chunk.js"
+  },
+  {
+    "revision": "208783fa02780d630bac",
+    "url": "/static/js/runtime~main.402cc814.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vihanr.github.io/precache-manifest.84173a74c9db39e24badffd83481150c.js"
+  "/precache-manifest.e30f76239581ca228d25e0ff42e4156d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vihanr.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
